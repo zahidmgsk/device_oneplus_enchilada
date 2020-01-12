9b209a31733f81b7bed633066f997c7b6cd7bc0a
Andrew Thomas <itz63c@statixos.com>
2020-01-12 13:36:56 -0500

    enchilada: override TARGET_OTA_ASSERT_DEVICE
    Signed-off-by: Andrew Thomas <itz63c@statixos.com>
    Change-Id: Id35b8cacbee98d214d825b062eb5d36c6f6f6aa5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,6 +19,9 @@
 
 DEVICE_PATH := device/oneplus/enchilada
 
+# Assertions
+TARGET_OTA_ASSERT_DEVICE := OnePlus6,oneplus6,enchilada
+
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 
