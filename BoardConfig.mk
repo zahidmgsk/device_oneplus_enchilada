7efe8ee41f28f30893abad9d19723cc9cdd05a9b
zahidmgsk <zahidmgsk@gmail.com>
2021-05-28 11:11:14 +0000

    enchilada: Move Kernel config to specific device tree 2/2 from device/oneplus/sdm845-common
    Signed-off-by: zahidmgsk <zahidmgsk@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,5 +22,8 @@ DEVICE_PATH := device/oneplus/enchilada
 # Assert
 TARGET_OTA_ASSERT_DEVICE := OnePlus6
 
+# Kernel
+TARGET_KERNEL_CONFIG := kronic_defconfig
+
 # inherit from the proprietary version
 -include vendor/oneplus/enchilada/BoardConfigVendor.mk
